dc231c0100e1c0ab5036743afb31f8f6dd923a6d
KHJGames <khjgames@gmail.com>
2026-04-15 03:50:15 -0400

    bugfix later

--- YgoDuelistCode/Cards/Monster/Todo/Effect/Rigorous_Reaver.cs
+++ YgoDuelistCode/Cards/Monster/Todo/Effect/Rigorous_Reaver.cs
@@ -36,7 +36,7 @@ public sealed class Rigorous_Reaver : EffectMonsterCard
     {
     }
     // Dictates the card pack tags this card will be included in.
-    public override YgoCardPackTags PackTags => YgoCardPackTags.Starter | YgoCardPackTags.Trap;
+    //public override YgoCardPackTags PackTags => YgoCardPackTags.Trap;
     // You will always see bundled cards when RNG rolls this card, but not the other way around.
     //public override Type[] BundledCards => new[]
     //{
--- YgoDuelistCode/Cards/Monster/Todo/Effect/Slate_Warrior.cs
+++ YgoDuelistCode/Cards/Monster/Todo/Effect/Slate_Warrior.cs
@@ -24,7 +24,7 @@ public sealed class Slate_Warrior : EffectMonsterCard
     {
     }
     // Dictates the card pack tags this card will be included in.
-    public override YgoCardPackTags PackTags => YgoCardPackTags.Starter | YgoCardPackTags.Burn;
+    //public override YgoCardPackTags PackTags => YgoCardPackTags.Starter | YgoCardPackTags.Burn;
     // You will always see bundled cards when RNG rolls this card, but not the other way around.
     //public override Type[] BundledCards => new[]
     //{
